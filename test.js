abc7ca33dcd5427d95f65dddfdf6434f0d33d187
anbang <79228807@qq.com>
2019-12-20 15:49:21 +0800

    第一版本移除

---
+++ test.js
@@ -0,0 +1,22 @@
+var Snowflake = require("./index");
+
+let idWorker = new Snowflake(1, 1);
+let id = idWorker.nextId();
+console.log(`id:${id} ${id.toString()}`)
+
+
+//约640-660ms
+//约850-1000ms
+console.time('100W时间')
+for (let i = 0, len = 10000 * 100; i < len; i++) {
+    id = idWorker.nextId();
+}
+console.timeEnd('100W时间');
+
+//约2600-2800ms
+//约3600-3850ms
+console.time('409.6W时间')
+for (let j = 0, len2 = 10000 * 409.6; j < len2; j++) {
+    id = idWorker.nextId();
+}
+console.timeEnd('409.6W时间');
